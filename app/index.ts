c9eba6e638157dd63747a1567de6a76051810ff8
Mr.x <313008430@qq.com>
2017-12-28 23:55:18 +0800

    Add files via upload

---
+++ app/index.ts
@@ -0,0 +1,14 @@
+import * as css from './css.css';
+
+import co from './css/style.css';
+
+import * as ccc from './css/t.css';
+
+console.log(ccc)
+
+console.log(co['co'])
+console.log(css)
+
+// let ss = require('./css.css')
+
+console.log(2);
